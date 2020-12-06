8fcd8859913cdc54260d41e6fddfeb44191c72b4
rubinak786 <70865586+rubinak786@users.noreply.github.com>
2020-12-06 16:48:08 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project27
+https://karpagam-r.github.io/Project-27-Newton-s-Cradle/
---
+++ Rope.js
@@ -0,0 +1,22 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+           pointB : pointB
+           
+        
+        }
+        
+        this.rope = Constraint.create(options);
+        this.pointB = pointB
+        World.add(world, this.rope);
+    }
+
+    display()
+    {
+        var pointA = this.rope.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
